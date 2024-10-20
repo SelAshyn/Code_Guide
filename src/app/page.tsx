e3c6076f5b9c7ac7f9f16d5d5a41c48563278d5f
Sel_Ashyn <110982820+SelAshyn@users.noreply.github.com>
2024-10-20 22:24:12 +0545

    Hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+import { Bebas_Neue, Arima } from 'next/font/google';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styleh.css";
+import Link from "next/link";
 
-export default function Home() {
+const bebas_neue = Bebas_Neue({
+  subsets: ['latin'],
+  weight: '400',  
+});
+
+const arima = Arima({
+  subsets: ['greek'],
+  weight: '400',
+})
+
+
+
+export default function Home(){
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <center>
+        <div className="main">
+          <div className="text">
+              <h1 style={bebas_neue.style} className='text-6xl text-yellow-300'><Link className='text-emerald-950' href="/">CODEGUIDE</Link> <br /> BEST PATHWAY FOR DEVELOPER'S</h1> <br />
+              <h3 className='text-xl'>Welcome to CodeGuide - your guide to the world of coding! Here you'll find a comprehensive set of courses that will teach you everything you need to know about HTML, CSS, Java, and Python. With our expert tutors and unique learning methods, you'll be well on your way to mastering the art of coding.</h3>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <br /><br /><br /><br />
+        <div className="main2">
+          <h1 style={bebas_neue.style} className='text-6xl text-yellow-300 text-center'><Link className='text-purple-600' href="/">WHY WE??</Link></h1> <br />
+          <h3 className='text-xl'>CodeGuide, your ultimate companion in the world of coding! Whether you’re a beginner taking your first steps or an experienced coder looking to expand your skillset, CodeGuide offers a structured and engaging path to learning. Our platform features comprehensive courses in HTML, CSS, Java, and Python, designed to help you build a solid foundation and advance to expert levels. <br /> At CodeGuide, we believe learning to code should be accessible, enjoyable, and rewarding. That’s why we combine expert-led instruction, hands-on projects, and interactive exercises to make the process as effective and exciting as possible. Start your journey with CodeGuide and unlock the doors to a world of coding opportunities! </h3>
+        </div>
+        <br /><br />
+        <div className="intro4">
+                        <br />
+                        <div className="container3">
+                                
+                                <h1 className="heading" style={bebas_neue.style}>Get Started Now!</h1>
+                            
+                                <div className="box-container" style={arima.style}>
+                            
+                                    <div className="box">
+                                        
+                                        <h3>Sign Up for a Free Trial</h3>
+                                        <p>Get access to a range of free courses and learning resources when you sign up for a free trial of CodeGuide.</p>
+                                        <br />
+                                        <a href="./Coming/index.html" className="btn">Sign Up Free</a>
+
+                                    </div>
+                            
+                                    <div className="box">
+                                        
+                                        <h3>Choose a Course</h3>
+                                        <p>Choose from our range of courses and learning paths, and start learning coding at your own pace. </p><br/>
+                                        <a href="./Coming/index.html" className="btn">View Courses</a>
+                                    </div>
+                            
+                                    <div className="box">
+                                        
+                                        <h3>Get in Touch</h3>
+                                        <p>Learn how to create a mobile app using Java. This course covers everything from user interface design to mobile app testing.</p><br />
+                                        <a href="./Coming/index.html" className="btn">Contact Us</a>
+                                    </div>
+                            
+                            
+                                </div>
+                            <br />
+                            </div>
+            </div>
+      </center>
+    </>
   );
 }
